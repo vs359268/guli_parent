bfe9ef8f26635c82028dd78501daa73705fe2729
vs359268 <543732623@qq.com>
2020-12-25 12:57:15 +0800

    version1.0.0

---
+++ common/service_base/src/main/java/com/atguigu/servicebase/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.atguigu.servicebase.exceptionhandler;
+
+
+import com.atguigu.commonutils.R;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    //指定出现什么异常执行
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public R error(Exception e){
+        e.printStackTrace();
+        return R.error().message("执行了全局异常处理");
+    }
+}
